c4682d35a1e25c15f34a92f2b24936550f2399cb
sualah-mtn-gh <88894532+sualah-mtn-gh@users.noreply.github.com>
2022-03-01 18:10:16 +0000

    first commit

---
+++ Singleton/RegularObject/src/blueButton.js
@@ -0,0 +1,6 @@
+import { counter } from "./counter";
+
+const button = document.getElementById("blue");
+button.addEventListener("click", () => {
+  console.log("Counter total: ", counter.increment());
+});
